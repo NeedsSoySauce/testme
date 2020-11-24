2a1076ea21b2b78d0f9ac062583e2ca45e91c865
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-11-25 02:39:02 +1300

    Fixed createsuperuser command requiring an email

--- testme_auth/tests/unit/test_models.py
+++ testme_auth/tests/unit/test_models.py
@@ -17,7 +17,7 @@ class UserModelTests(TestCase):
     def test_create_user_no_email(self):
         """ Creating a user with no email works and stores NULL as their email in the database. """
         user = get_user_model().objects.create_user(self.username, None, self.password)
-        self.assertEqual(user.email, None)
+        self.assertIsNone(user.email)
 
     def test_create_user_duplicate_email(self):
         """ Creating a user with an email that's in use fails. """
@@ -25,3 +25,17 @@ class UserModelTests(TestCase):
 
         with self.assertRaises(IntegrityError):
             _ = get_user_model().objects.create_user(self.username, self.email, self.password)
+
+    def test_create_superuser(self):
+        """ Creating a superuser with an email works. """
+        user = get_user_model().objects.create_superuser(self.username, self.email, self.password)
+        self.assertEqual(user.email, self.email)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+    def test_create_superuser_no_email(self):
+        """ Creating a superuser without an email works and stores NULL as their email in the database. """
+        user = get_user_model().objects.create_superuser(self.username, password=self.password)
+        self.assertIsNone(user.email)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)

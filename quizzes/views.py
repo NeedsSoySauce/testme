946c0a58073fb1a23c54db68138cf78ee1f2351a
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-12-19 19:17:50 +1300

    Added search filter

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,3 +1,5 @@
+from rest_framework import filters
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Quiz, Question, Answer, Tag
@@ -10,6 +12,8 @@ class TagViewSet(ModelViewSet):
     """ Allows tags to be viewed or edited. """
     queryset = Tag.objects.all().order_by('pk')
     serializer_class = TagSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 
 class QuestionViewSet(UserLinkedModelViewSet):
@@ -17,6 +21,8 @@ class QuestionViewSet(UserLinkedModelViewSet):
     queryset = Question.objects.all().order_by('pk')
     serializer_class = QuestionSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['creator__username', 'creator__email', 'text', 'description', 'tags__name']
 
 
 class AnswerViewSet(UserLinkedModelViewSet):
@@ -24,6 +30,9 @@ class AnswerViewSet(UserLinkedModelViewSet):
     queryset = Answer.objects.all().order_by('pk')
     serializer_class = AnswerSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['creator__username', 'creator__email', 'question__text', 'question__description',
+                     'question__tags__name', 'text']
 
 
 class QuizViewSet(UserLinkedModelViewSet):
@@ -31,3 +40,6 @@ class QuizViewSet(UserLinkedModelViewSet):
     queryset = Quiz.objects.all().order_by('pk')
     serializer_class = QuizSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['creator__username', 'creator__email', 'name', 'description', 'questions__text',
+                     'questions__description', 'questions__tags__name']

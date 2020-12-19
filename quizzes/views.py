cd39bede37e5da2718a1b76505e218fc4e2a1c8e
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-12-19 18:57:32 +1300

    Order viewset results for pagination

--- quizzes/views.py
+++ quizzes/views.py
@@ -8,26 +8,26 @@ from .viewsets import UserLinkedModelViewSet
 
 class TagViewSet(ModelViewSet):
     """ Allows tags to be viewed or edited. """
-    queryset = Tag.objects.all()
+    queryset = Tag.objects.all().order_by('pk')
     serializer_class = TagSerializer
 
 
 class QuestionViewSet(UserLinkedModelViewSet):
     """ Allows questions to be viewed or edited. """
-    queryset = Question.objects.all()
+    queryset = Question.objects.all().order_by('pk')
     serializer_class = QuestionSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
 
 
 class AnswerViewSet(UserLinkedModelViewSet):
     """ Allows answers to be viewed or edited. """
-    queryset = Answer.objects.all()
+    queryset = Answer.objects.all().order_by('pk')
     serializer_class = AnswerSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
 
 
 class QuizViewSet(UserLinkedModelViewSet):
     """ Allows quizzes to be viewed or edited. """
-    queryset = Quiz.objects.all()
+    queryset = Quiz.objects.all().order_by('pk')
     serializer_class = QuizSerializer
     permission_classes = [IsCreatorOrAdminUserOrReadOnly]
